e4ba320f2cc04b095fcc3a7b990f6f6c6a2df58d
Choi Jin Young <choijy613@gmail.com>
2020-04-30 23:58:21 +0900

    0430 Team Project Homework

---
+++ UI/Assets/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject option;
+
+
+    public void PressSettings()
+    {
+        option.SetActive(true);
+    }
+
+    public void Return()
+    {
+        option.SetActive(false);
+    }
+
+    public void PressStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PressBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
